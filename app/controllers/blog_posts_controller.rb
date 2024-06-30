4855bb4f5ca0ff17df4c68ad91874b743ff55b3c
Mahfujur Rahman Shohan <mahfujurrahmancityu@gmail.com>
2024-06-30 13:06:18 +0600

    creating new blog done

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -8,5 +8,25 @@ class BlogPostsController < ApplicationController
     end
 
     def new
+        @blog = BlogPost.new
+    end
+
+    def create
+        @blog = BlogPost.new(blog_params)
+
+        if @blog.save
+            redirect_to @blog
+        else
+            render :new
+        end
+    end
+
+    def edit
+    end
+
+    private
+
+    def blog_params
+        params.require(:blog_post).permit(:title, :body)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  root "blog_posts#index"
-  get "blog_posts/new", to: "blog_posts#new", as: "new_blog"
+  
+  get "/blog_posts/new", to: "blog_posts#new", as: "new_blog"
+  
+  get "/blog_posts/:id", to: "blog_posts#show", as: "blog_post"
+  get "/blog_posts/:id/edit", to: "blog_posts#edit", as: "edit_blog"
+  post "/blog_posts", to: "blog_posts#create"
 
-  get "blog_posts/:id", to: "blog_posts#show", as: "blog_post"
+  root "blog_posts#index"
+  
 
   
 end
